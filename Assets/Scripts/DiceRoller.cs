0af89745727970ec56f8289266032f06cc1f40f0
Andrei Serban <andreiserbanrazvan@gmail.com>
2020-12-29 21:02:13 +0200

    dev commit
    commit

---
+++ Assets/Scripts/DiceRoller.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceRoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DiceValues = new int[4];
+    }
+
+    public Sprite[] DiceImageOne;
+    public Sprite[] DiceImageZero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool doneRolling = false;
+
+    public int[] DiceValues;
+    public int DiceTotal;
+    
+
+
+    public void NewTurn()
+    {
+        //This is the start of the player turn
+        // We dont have a roll for them yet.
+
+        doneRolling = false;
+
+    }
+            
+    public void RollTheDice()
+    {
+        // In Ur , you roll four dice (classically Tetrahedron),
+        // have half their face have a value of "1" and half a value of zero
+
+        DiceTotal = 0;
+        for (int i = 0; i < DiceValues.Length; i++)
+        {
+            DiceValues[i] = Random.Range(0 , 2);
+            DiceTotal += DiceValues[i];
+
+
+            if(DiceValues[i] == 0)
+            {
+                this.transform.GetChild(i).GetComponent<Image>().sprite = 
+                    DiceImageZero[Random.Range(0,DiceImageZero.Length)];
+            }
+            else
+            {
+                this.transform.GetChild(i).GetComponent<Image>().sprite =
+                    DiceImageOne[Random.Range(0, DiceImageOne.Length)];
+            }
+
+            doneRolling = true;
+            
+        }
+
+        // Update the visuals to show the dice roll
+        // 
+
+       // Debug.Log("Rolled: " + DiceTotal);
+
+    }
+}
---
+++ Assets/Scripts/PlayerStone.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //theDiceRoller = GameObject.FindObjectOfType<DiceRoller>();
+    }
+
+    public Tile StartingTile;
+    Tile currentTile;
+
+    public DiceRoller theDiceRoller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Click");
+
+        if (theDiceRoller.doneRolling == false)
+        {
+            // We can't move yet.
+            return;
+        }
+
+        int spacesToMove = theDiceRoller.DiceTotal;
+
+        // where should we end up
+
+        if(spacesToMove == 0)
+        {
+            return;
+        }
+
+        Tile finalTile = currentTile;
+
+        for (int i = 0; i < spacesToMove; i++)
+        {
+            if (finalTile == null)
+            {
+                finalTile = StartingTile;
+            }
+            else
+            {
+                finalTile = finalTile.NextTiles[0]; 
+            }
+
+        }
+
+        if(finalTile == null)
+        {
+            // we moved an off board tile by zero
+            return;
+        }
+
+        // teleport the tile to final tile
+
+        // todo ANIMATE !
+
+        this.transform.position = finalTile.transform.position;
+        
+        currentTile = finalTile;
+       
+    }
+}
